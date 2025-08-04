97abfa748cbd2b289b5db985173b10b4aa207121
Fernando Hernández Joaquín <147170143+FeHeJogithub@users.noreply.github.com>
2025-08-04 08:56:02 -0400

    Add files via upload
    ApiRest con go

--- main.go
+++ main.go
@@ -1,117 +1,161 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"go-mysql/database"
-	"go-mysql/handlers"
-	"go-mysql/models"
-	"log" //indica un registro con fecha y hora
-	"os"
-	"strings"
+	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+type album struct {
+	ID     string  `json:"id`
+	Title  string  `json:"title`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
 
-	//fmt.Println("Mysql y go con fehejo")
+var albums = []album{
 
-	db, err := database.Connect()
-	if err != nil {
-		log.Fatal(err)
-	}
+	{ID: "1", Title: "Blue Train", Artist: "John Contrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 50.99},
+}
 
-	defer db.Close()
-
-	//handlers.DeleteContact(db, 5)
-	//Crear una instancia de Contact
-	/*newContact := models.Contact{
-		Id:    4,
-		Name:  "Fehejo",
-		Email: "fhernandez1@hotmail.com",
-		Phone: "123456789",
-	}*/
-
-	//handlers.UpdateContact(db, newContact)
-	//handlers.CreateContact(db, newContact)
-	//handlers.ListContacts(db)
-
-	//handlers.GetContactByID(db, 3)
-
-	for {
-		fmt.Println("\nMenu: ")
-		fmt.Println("1. Listar contactos")
-		fmt.Println("2. Obtener contacto por ID")
-		fmt.Println("3. Crear nuevo contacto")
-		fmt.Println("4. Actualizar contacto")
-		fmt.Println("5. Eliminar contacto")
-		fmt.Println("6. Salir")
-		fmt.Println("Seleccione una opcion: ")
-
-		var option int
-		fmt.Scanln(&option)
-
-		switch option {
-		case 1:
-			handlers.ListContacts(db)
-
-		case 2:
-			fmt.Print("Ingrese el ID del contacto: ")
-			var idContact int
-			fmt.Scanln(&idContact)
-			handlers.GetContactByID(db, idContact)
-		case 3:
-			newContact := inputContactDetails(option)
-			handlers.CreateContact(db, newContact)
-			handlers.ListContacts(db)
-		case 4:
-			updateContact := inputContactDetails(option)
-			handlers.UpdateContact(db, updateContact)
-			handlers.ListContacts(db)
-		case 5:
-			fmt.Println("Ingrese el ID del contacto que quiere eliminar: ")
-			var idContact int
-			fmt.Scanln(&idContact)
-			handlers.DeleteContact(db, idContact)
-		case 6:
-			fmt.Println("Saliendo del programa...")
-			return
-		default:
-			fmt.Println("Opcion no valida. por favor, selecciones una opcion valida ")
-
-		}
-
-	}
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 
 }
 
-func inputContactDetails(option int) models.Contact {
+func postAlbums(c *gin.Context) {
+	var newAlbum album
 
-	reader := bufio.NewReader(os.Stdin)
+	if err := c.BindJSON(&newAlbum); err != nil {
 
-	var contact models.Contact
+		return
+	}
+	albums = append(albums, newAlbum)
 
-	if option == 4 {
-		fmt.Print("Ingrese el ID del contacto que quiere editar: ")
-		var idContact int
-		fmt.Scanln(&idContact)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 
-		contact.Id = idContact
+}
+
+func getAlbumsByID(c *gin.Context) {
+
+	id := c.Param("id")
+
+	for _, a := range albums {
+
+		if a.ID == id {
+
+			c.IndentedJSON(http.StatusOK, a)
+			return
+
+		}
 	}
 
-	fmt.Print("Ingrese el nombre del contacto: ")
-	name, _ := reader.ReadString('\n')
-	contact.Name = strings.TrimSpace(name)
+	c.IndentedJSON(http.StatusNotFound, gin.H{
 
-	fmt.Print("Ingrese el correo del contacto: ")
-	email, _ := reader.ReadString('\n')
-	contact.Email = strings.TrimSpace(email)
+		"message": "album no encontrado"})
 
-	fmt.Print("Ingrese el telefono del contacto: ")
-	phone, _ := reader.ReadString('\n')
-	contact.Phone = strings.TrimSpace(phone)
+}
+func putAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
 
-	return contact
+	var updatedAlbum album
+	if err := c.BindJSON(&updatedAlbum); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "JSON inválido"})
+		return
+	}
 
+	for i, a := range albums {
+		if a.ID == id {
+			// Actualiza el álbum
+			updatedAlbum.ID = id // Mantiene el ID original
+			albums[i] = updatedAlbum
+
+			c.IndentedJSON(http.StatusOK, updatedAlbum)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "álbum no encontrado"})
+}
+
+func deleteAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range albums {
+		if a.ID == id {
+			// Eliminar el álbum del slice
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "álbum eliminado"})
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "álbum no encontrado"})
+}
+
+/*func deleteAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
+
+	var updatedAlbum album
+	if err := c.BindJSON(&updatedAlbum); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "JSON inválido"})
+		return
+	}
+
+	for i, a := range albums {
+		if a.ID == id {
+			// Actualiza el álbum
+			updatedAlbum.ID = id // Mantiene el ID original
+			albums[i] = updatedAlbum
+
+			c.IndentedJSON(http.StatusOK, updatedAlbum)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "álbum no encontrado"})
+}
+
+/*
+func putAlbumsByID(c *gin.Context) {
+
+	id := c.Param("id")
+
+	for _, a := range albums {
+
+		if a.ID == id {
+
+			c.IndentedJSON(http.StatusOK, a)
+			return
+
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{
+
+		"message": "album no encontrado"})
+
+}*/
+
+func main() {
+
+	//fmt.Println("Bienvenido a vinyl-api con fehejo")
+
+	router := gin.Default()
+
+	//router.GET("/", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+
+	//	"message": "Hola mundo con fehejo",
+	//})
+	//})
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumsByID)
+	router.POST("/albums", postAlbums)
+	router.PUT("/albums/:id", putAlbumsByID)
+	router.DELETE("/albums/:id", deleteAlbumsByID)
+
+	router.Run("localhost:8080")
 }
